c5f5417c2a8ccae84329e18c8dfe16c8c7beb878
AnnaLotz <anna.lotz@web.de>
2019-10-15 17:23:06 +0200

    End of lesson 3

--- L02_FirstFudge/Main.js
+++ L02_FirstFudge/Main.js
@@ -1,13 +1,30 @@
 "use strict";
+///<reference types="../Fudge/FudgeCore.js"/> 
 var L02_FirstFudge;
+///<reference types="../Fudge/FudgeCore.js"/> 
 (function (L02_FirstFudge) {
-    var fudge = FudgeCore;
+    var f = FudgeCore;
     window.addEventListener("load", handleLoad);
     function handleLoad(_event) {
         const canvas = document.querySelector("canvas");
-        console.log(canvas);
-        let viewport = new fudge.Viewport();
-        viewport.initialize("Viewport", null, null, canvas);
+        f.RenderManager.initialize();
+        f.Debug.log(canvas);
+        let node = new f.Node("Quad");
+        let mesh = new f.MeshQuad();
+        let cmpMesh = new f.ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+        let mtrSolidWhite = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        let cmpMTR = new f.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMTR);
+        //Camera
+        let camera = new f.Node("Camera");
+        let cmpCam = new f.ComponentCamera();
+        camera.addComponent(cmpCam);
+        cmpCam.pivot.translateZ(2); //Camera bewegen (um 2 auf der Z-Achse)
+        L02_FirstFudge.viewport = new f.Viewport();
+        L02_FirstFudge.viewport.initialize("Viewport", node, camera.getComponent(f.ComponentCamera), canvas);
+        f.Debug.log(L02_FirstFudge.viewport);
+        L02_FirstFudge.viewport.draw();
     }
 })(L02_FirstFudge || (L02_FirstFudge = {}));
 //# sourceMappingURL=Main.js.map
--- L02_FirstFudge/Main.ts
+++ L02_FirstFudge/Main.ts
@@ -1,12 +1,34 @@
+///<reference types="../Fudge/FudgeCore.js"/> 
 namespace L02_FirstFudge {
-    import fudge = FudgeCore;
+    import f = FudgeCore;
+    export let viewport: f.Viewport;
     window.addEventListener("load", handleLoad);
 
     function handleLoad(_event: Event): void {
         const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        console.log(canvas);
-        console.log(fudge);
-        // let viewport: fudge.Viewport = new fudge.Viewport();
-        // viewport.initialize("Viewport", null, null, canvas);
-    }
+        f.RenderManager.initialize();
+        f.Debug.log(canvas);
+
+        let node: f.Node = new f.Node("Quad");
+
+        let mesh: f.MeshQuad = new f.MeshQuad();
+        let cmpMesh: f.ComponentMesh = new f. ComponentMesh(mesh);
+        node.addComponent(cmpMesh);
+
+        let mtrSolidWhite: f.Material = new f.Material("SolidWhite", f.ShaderUniColor, new f.CoatColored(new f.Color(1, 1, 1, 1)));
+        let cmpMTR: f.ComponentMaterial = new f.ComponentMaterial(mtrSolidWhite);
+        node.addComponent(cmpMTR);
+
+        //Camera
+        let camera: f.Node = new f.Node("Camera");
+        let cmpCam: f.ComponentCamera = new f.ComponentCamera();
+        camera.addComponent(cmpCam);
+        cmpCam.pivot.translateZ(2); //Camera bewegen (um 2 auf der Z-Achse)
+ 
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", node, camera.getComponent(f.ComponentCamera), canvas);
+        f.Debug.log(viewport);
+        
+        viewport.draw();
+    }   
 }
