f20f382a4d37ddb0342ed49b109dec3f52aab712
AnnaLotz <anna.lotz@web.de>
2019-11-21 15:00:17 +0100

    Projekt für Praktikum angelegt

---
+++ L08_FudgeCraft_Collision/Classes/Fragment.ts
@@ -0,0 +1,88 @@
+namespace L08_FudgeCraft_Collision {
+    import f = FudgeCore;
+
+    export class Fragment extends f.Node {
+
+        fragmentDef: number[][][] = this.getShapeArray();
+        shape: number[][];
+        position: f.Vector3 = new f.Vector3(0, 0, 0);
+        materials: f.Material[] = this.createMaterials();
+        cubes: Cube[] = [];
+
+
+        constructor(_shape: number) {
+            super("Fragment-Type" + _shape);
+            this.shape = this.fragmentDef[_shape];
+            let mtr: f.Material = this.getMaterial(_shape);
+
+            //schleife durch eine Form um die einzelnen Cubes zu setzen
+            for (let position of this.shape) {
+                //einen Nullvektor erstellen an dessen Werte die Position gesetzt wird
+                let vctPosition: f.Vector3 = f.Vector3.ZERO();
+                vctPosition.set(position[0], position[1], position[2]);
+                //neuen Cube an der vectorPosition erstellen und anhängen
+                let cube: Cube = new Cube(vctPosition, mtr);
+                this.cubes.push(cube);
+                this.appendChild(cube);
+            }
+        } //close constructor
+
+
+        getCubesPositions(): number[][] {
+            let cubePositions: number[][] = [];
+            for (let i: number = 0; i < this.cubes.length; i++) {
+                let cube: Cube = this.cubes[i];
+                cubePositions[i] =
+                    [
+                        Math.round(cube.mtxWorld.translation.x),
+                        Math.round(cube.mtxWorld.translation.y),
+                        Math.round(cube.mtxWorld.translation.z)
+                    ];
+            }
+            return cubePositions;
+        } //close getCubesPositions
+
+
+
+        getShapeArray(): number[][][] {
+            //[Index der Form/shape][Eine Form][Position eines Cubes]
+            let shapeArray: number[][][] =
+                [
+                    // [[0, 0, 0], [1, 0, 0]],
+                    // [[0, 0, 0], [1, 0, 0]],
+                    [[-1, 0, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]], //L: Orange Ricky
+                    [[-1, -1, 0], [-1, 0, 0], [0, 0, 0], [1, 0, 0]], //J: Blue Ricky
+                    [[0, 0, 0], [0, 1, 0], [-1, 1, 0], [1, 0, 0]], //Z: Cleveland Z
+                    [[0, 0, 0], [0, 1, 0], [1, 1, 0], [-1, 0, 0]], //S: Rhode Island Z
+                    [[0, 0, 0], [0, 1, 0], [-1, 0, 0], [1, 0, 0]], //T: Teewee 
+                    [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 0]], //O: Smashboy
+                    [[0, 0, 0], [1, 0, 0], [2, 0, 0], [-1, 0, 0]] // I: Hero
+                ];
+            return shapeArray;
+        } //close getShapeArray
+
+
+        createMaterials(): f.Material[] {
+            let mtrArray: f.Material[] = [];
+            mtrArray = [
+                new f.Material("Orange", f.ShaderFlat, new f.CoatColored(new f.Color(1, 0.75, 0, 1))),
+                new f.Material("Blue", f.ShaderFlat, new f.CoatColored(f.Color.BLUE)),
+                new f.Material("Red", f.ShaderFlat, new f.CoatColored(f.Color.RED)),
+                new f.Material("Green", f.ShaderFlat, new f.CoatColored(f.Color.GREEN)),
+                new f.Material("Magenta", f.ShaderFlat, new f.CoatColored(f.Color.MAGENTA)),
+                new f.Material("Yellow", f.ShaderFlat, new f.CoatColored(f.Color.YELLOW)),
+                new f.Material("Cyan", f.ShaderFlat, new f.CoatColored(f.Color.CYAN))
+            ];
+            return mtrArray;
+        } //close createMaterials
+
+
+        getMaterial(_shape: number): f.Material {
+            let mtr: f.Material = this.materials[_shape];
+            return mtr;
+        } //close getMaterial
+
+
+    } //close class Fragment
+
+} //namespace zu
---
+++ L08_FudgeCraft_Collision/Main.ts
@@ -0,0 +1,150 @@
+///<reference types="../Fudge/FudgeCore.js"/> 
+namespace L08_FudgeCraft_Collision {
+    import f = FudgeCore;
+    export let viewport: f.Viewport;
+
+    document.addEventListener("DOMContentLoaded", handleLoad);
+
+    let game: f.Node;
+    let fragment: Fragment;
+    let currentFragment: Fragment;
+    let firstFragment: Fragment;
+
+
+    // ############################################################################################
+    // ############################################################################################
+    function handleLoad(_event: Event): void {
+        console.log("Hello World");
+        const canvas: HTMLCanvasElement = document.querySelector("canvas");
+        f.RenderManager.initialize(true); //true um Antialiasing zu vermeiden
+
+        //Camera
+        let camera: f.Node = new f.Node("Camera");
+        let cmpCam: f.ComponentCamera = new f.ComponentCamera();
+        camera.addComponent(cmpCam);
+        cmpCam.pivot.translate(new f.Vector3(0, 3, 30)); // kamera auf ort setzen
+        // cmpCam.pivot.lookAt(f.Vector3.ZERO()); // um auf 0|0|0 zu schauen
+
+        //create Game Node
+        game = createGame();
+
+        //Light
+        let cmpLight: f.ComponentLight = new f.ComponentLight(new f.LightDirectional(f.Color.WHITE));
+        cmpLight.pivot.lookAt(new f.Vector3(0.5, 0, 0.5));
+        game.addComponent(cmpLight);
+
+        //Viewport
+        viewport = new f.Viewport();
+        viewport.initialize("Viewport", game, camera.getComponent(f.ComponentCamera), canvas);
+        viewport.draw();
+        console.log("currentFragment after viewport.draw(): " + currentFragment.getCubesPositions()); //erst hier ist die position im raum richtig erfasst
+
+        window.addEventListener("keydown", handleKeyDown);
+
+        console.log("Setup done");
+    } //close handleLoad
+
+
+    function createGame(): f.Node {
+        game = new f.Node("Game");
+
+        fragment = new Fragment(0);
+        firstFragment = fragment;
+        game.appendChild(fragment);
+
+        fragment = new Fragment(3);
+        fragment.addComponent(new f.ComponentTransform(f.Matrix4x4.TRANSLATION(new f.Vector3(0, 3, 0))));
+        currentFragment = fragment;
+        game.appendChild(fragment);
+
+        return game;
+    } //close createGame
+
+
+    // ############################################################################################
+    // ############################################################################################
+    function handleKeyDown(_event: KeyboardEvent): void {
+        
+        processInput(_event, 1);
+
+        if (checkIfHit()) {
+            processInput(_event, -1); //die bewegung rückgängig machen
+            createNewFragment(); //neues Fragment erstellen und zum currentFrag machen
+        }
+    } //close handleKeyDown
+
+
+    function checkIfHit(): boolean {
+
+        console.log("firstFrag: " + firstFragment.getCubesPositions());
+        console.log("currentFrag: " + currentFragment.getCubesPositions());
+
+        for (let fragment of game.getChildren()) {
+            // console.log("1. durch alle game.getChildren");
+            if (fragment != currentFragment && fragment instanceof Fragment) {
+                // console.log("2. if fragment != currentFragment und Fragment instace");
+                for (let othersPosition of fragment.getCubesPositions()) {
+                    // console.log("3. for otherpositions of fragment.getCubesPositions");
+                    for (let currentPosition of currentFragment.getCubesPositions()) {
+                        // console.log("4. for currentPos of currentFrag.getCubesPositions");
+                        // console.log(othersPosition);
+                        // console.log(currentPosition);
+                        if (othersPosition[0] == currentPosition[0] && othersPosition[1] == currentPosition[1]) {
+                            console.log("HIT!");
+                            return true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    } //close checkIfHit
+
+
+    function createNewFragment(): void {
+        //funktioniert noch nicht: das neue Fragment ist nicht sichtbar, hat aber werte
+        fragment = new Fragment(0);
+        fragment.addComponent(new f.ComponentTransform);
+        fragment.cmpTransform.local.translate(new f.Vector3(0, 5, 0));
+        currentFragment = fragment;
+        game.appendChild(fragment);
+        
+        f.RenderManager.update();
+        viewport.draw();
+    } //close createNewFragment
+
+
+    function processInput(_event: KeyboardEvent, _moveOn: number): void {
+
+        //bewegung
+        switch (_event.code) {
+            case f.KEYBOARD_CODE.W: //W später raus nehmen
+                currentFragment.cmpTransform.local.translateY(_moveOn * 1);
+                break;
+            case f.KEYBOARD_CODE.A:
+                currentFragment.cmpTransform.local.translateX(_moveOn * -1);
+                break;
+            case f.KEYBOARD_CODE.D:
+                currentFragment.cmpTransform.local.translateX(_moveOn * 1);
+                break;
+            case f.KEYBOARD_CODE.S:
+                currentFragment.cmpTransform.local.translateY(_moveOn * -1);
+                break;
+        }
+        //Rotation
+        switch (_event.code) {
+            case f.KEYBOARD_CODE.Q:
+                currentFragment.cmpTransform.local.rotateZ(_moveOn * 90);
+                break;
+            case f.KEYBOARD_CODE.E:
+                currentFragment.cmpTransform.local.rotateZ(_moveOn * -90);
+                break;
+        }
+
+        f.RenderManager.update();
+        viewport.draw();
+    } //close process Input
+
+
+} //close Namespace
